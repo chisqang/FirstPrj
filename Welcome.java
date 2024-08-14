c9f72f598d8d83b62d802a19839d33a0af81e3c5
Quang Cao <caoquang1117@gmail.com>
2024-08-14 11:19:53 +0700

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Cao Chi Quang");
+        System.out.println("Welcome to the DHKTPM19ATT");
+    }
+}
