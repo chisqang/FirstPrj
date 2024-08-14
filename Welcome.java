f4dcf91553b16994bbc49d69a68d53e9357af20b
Quang Cao <caoquang1117@gmail.com>
2024-08-14 11:22:35 +0700

    Delete Welcome.java

--- Welcome.java
+++
@@ -1,6 +0,0 @@
-public class Welcome {
-    public static void main(String[] args) {
-        System.out.println("Hello Cao Chi Quang");
-        System.out.println("Welcome to the DHKTPM19ATT");
-    }
-}
